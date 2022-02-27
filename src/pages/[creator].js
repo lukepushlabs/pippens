998a32def55fa671cd87f9db67deb1ee73b1bd7b
Nick Confrey <nickconfrey@earthlink.net>
2022-02-27 15:51:26 -0500

    get rest of the data about creator coins, display on creator page

--- src/pages/[creator].js
+++ src/pages/[creator].js
@@ -1,14 +1,15 @@
 import React from "react";
 import Meta from "components/Meta";
 
-function CreatorPage({ creator, priceInUSD, priceInRLY, rallyError }) {
+function CreatorPage({ priceInUSD, priceInRLY, rallyJSON, rallyError }) {
   return (
     <>
       <Meta />
-      <h1>Creator: {creator}</h1>
+      <img src={rallyJSON.imageUrl} alt="Logo" />
+      <h1>Creator: {rallyJSON.name}, Token: {rallyJSON.symbol}</h1>
       <h1>Token Price: {priceInUSD}</h1>
       <h1>Token Price in Rally: {priceInRLY}</h1>
-      <h1>Error: {rallyError}</h1>
+      <h1>Rally since {rallyJSON.launchTimestamp}</h1>
     </>
   );
 }
@@ -17,15 +18,19 @@ CreatorPage.getInitialProps = async (ctx) => {
   let creator = ctx.query.creator;
 
   let symbol = new String(creator).toLowerCase()
-  const rallyData = await fetch(`https://api.rally.io/v1/creator_coins/${symbol}/price`)
+  const priceData = await fetch(`https://api.rally.io/v1/creator_coins/${symbol}/price`)
+  const rallyData = await fetch(`https://api.rally.io/api/creator-coins/${symbol}`)
+  const priceJSON = await priceData.json()
   const rallyJSON = await rallyData.json()
-  let priceInUSD = parseFloat(rallyJSON.priceInUSD).toFixed(2)
-  let priceInRLY = parseFloat(rallyJSON.priceInRLY).toFixed(2)
+  let priceInUSD = parseFloat(priceJSON.priceInUSD).toFixed(2)
+  let priceInRLY = parseFloat(priceJSON.priceInRLY).toFixed(2)
+
   let rallyError = rallyJSON.errorMessage
   console.log(rallyError)
   console.log(rallyJSON)
+  console.log(priceJSON)
 
-  return { creator, priceInUSD, priceInRLY, rallyError };
+  return { priceInUSD, priceInRLY, rallyJSON, rallyError };
 }
 
 export default CreatorPage;
