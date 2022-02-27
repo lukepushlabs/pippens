25b8d0a88b14878d4a095754dcdb603072cd3e57
Nick Confrey <nickconfrey@earthlink.net>
2022-02-27 15:16:40 -0500

    use Rally REST endpoint to get token price for each creator page

--- src/pages/[creator].js
+++ src/pages/[creator].js
@@ -1,20 +1,31 @@
 import React from "react";
 import Meta from "components/Meta";
 
-function CreatorPage({creator}) {
+function CreatorPage({ creator, priceInUSD, priceInRLY, rallyError }) {
   return (
     <>
       <Meta />
-      <h1>Test-{creator}</h1>
+      <h1>Creator: {creator}</h1>
+      <h1>Token Price: {priceInUSD}</h1>
+      <h1>Token Price in Rally: {priceInRLY}</h1>
+      <h1>Error: {rallyError}</h1>
     </>
   );
 }
 
 CreatorPage.getInitialProps = async (ctx) => {
   let creator = ctx.query.creator;
-  const res = await fetch('https://api.github.com/repos/vercel/next.js')
-  const json = await res.json()
-  return { stars: json.stargazers_count, creator};
+
+  let symbol = new String(creator).toLowerCase()
+  const rallyData = await fetch(`https://api.rally.io/v1/creator_coins/${symbol}/price`)
+  const rallyJSON = await rallyData.json()
+  let priceInUSD = parseFloat(rallyJSON.priceInUSD).toFixed(2)
+  let priceInRLY = parseFloat(rallyJSON.priceInRLY).toFixed(2)
+  let rallyError = rallyJSON.errorMessage
+  console.log(rallyError)
+  console.log(rallyJSON)
+
+  return { creator, priceInUSD, priceInRLY, rallyError };
 }
 
 export default CreatorPage;
